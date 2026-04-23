879a5a1c1a85bcd1fc9f10c2728ea093b55d9df6
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-04-23 10:42:15 +0900

    feat: initial project structure (frontend, backend, app + gitignore)

---
+++ next502_backend/src/main/java/bitc/next502/next502_backend/Next502BackendApplication.java
@@ -0,0 +1,13 @@
+package bitc.next502.next502_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Next502BackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Next502BackendApplication.class, args);
+  }
+
+}
---
+++ next502_backend/src/test/java/bitc/next502/next502_backend/Next502BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.next502.next502_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Next502BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
