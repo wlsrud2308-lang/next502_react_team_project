ee308af3e26ed87c408d69a47f3a34d3a510e37f
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-04-23 12:07:06 +0900

    프론트 엔드 기초 경로 설정

---
+++ next502_frontend/src/components/auth/Login.jsx
@@ -0,0 +1,16 @@
+//  File: Login.jsx
+//  User: it
+//  Date: 2026-04-23
+//  Time: 오후 12:05
+//  Desc : 
+
+function Login(props) {
+
+    return (
+        <div>
+            <h1>제목</h1>
+        </div>
+    );
+}
+
+export default Login;
---
+++ next502_frontend/src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+//  File: Header.jsx
+//  User: it
+//  Date: 2026-04-23
+//  Time: 오후 12:06
+//  Desc : 
+
+function Header(props) {
+
+    return (
+        <div>
+            <h1>제목</h1>
+        </div>
+    );
+}
+
+export default Header;
