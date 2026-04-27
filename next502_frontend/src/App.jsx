21a14e744e135ab29434bebf27bb82ea02fbe7ac
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-04-27 17:20:41 +0900

    네이버 지도 등록 최종 완료

--- next502_frontend/src/App.jsx
+++ next502_frontend/src/App.jsx
@@ -1,11 +1,9 @@
 // src/App.jsx
 import WarehouseSearchPage from './components/warehouse/WarehouseSearchPage';
-import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 CSS 확인!
 
 function App() {
   return (
     <div className="App">
-      {/* 만약 레이아웃 헤더가 있다면 <Header /> 를 위에 두세요 */}
       <WarehouseSearchPage />
     </div>
   );
