2ba56df9195224d9fd64c2884ea97dec8d340339
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-04-27 15:40:08 +0900

    지도 관련 연결

--- next502_frontend/src/main.jsx
+++ next502_frontend/src/main.jsx
@@ -1,16 +1,13 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { NavermapsProvider } from 'react-naver-maps';
 
-const clientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
-
-// 만약 clientId가 undefined로 찍힌다면 .env 파일을 못 읽고 있는 겁니다.
-console.log('네이버 클라이언트 ID 확인:', clientId);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <NavermapsProvider ncpClientId={clientId}>
-    <App />
-  </NavermapsProvider>,
+  <React.StrictMode>
+    {/* 여기서 ncpClientId를 한 번만 선언합니다 */}
+    <NavermapsProvider ncpClientId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}>
+      <App />
+    </NavermapsProvider>
+  </React.StrictMode>,
 );
